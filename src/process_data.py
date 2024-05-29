a3a3e2d77bea11fe4f977d351cd4de2b2d0acc09
karenrivgi <karenrivgi@gmail.com>
2024-05-29 13:01:22 -0500

    Procesar datos (keypoints) y asignar labels

--- src/process_data.py
+++ src/process_data.py
@@ -4,50 +4,74 @@ import os
 # Procesamiento de datos
 import mediapipe as mp
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
+
+# -----------------------------------------------------------
 DATA_PATH = './data'
-
-import mediapipe as mp
+PROCESS_DATA_PATH = './processed_data'
 
 # Usar mediapipe para encontrar las landmarks 
 mp_holistic = mp.solutions.holistic # Holistic model (Detectar los landmarks)
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities (Dibujar los landmarks)
+mp_holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # conversion de color BGR a RGB *OpenCV tiene formato BGR
-    image.flags.writeable = False                  # Image is no longer writeable
-    results = model.process(image)                 # Hacer prediccion con el modelo pasado
-    image.flags.writeable = True                   # Image is now writeable 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
-    return image, results
 
-def draw_styled_landmarks(image, results):
-    # Dibujar landmarks de la cara
+# De cada frame extraeremos únicamente la info importante: los landmarks o puntitos
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
 
-    '''
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
-                             # Formato para los puntos
-                             mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=1, circle_radius=1), 
 
-                             # Formato para las conexiones
-                             mp_drawing.DrawingSpec(color=(0,128,0), thickness=1, circle_radius=1)
-                             ) 
-    
-    '''
-    # Dibujar landmarks de la pose
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                             mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=1, circle_radius=1), 
-                             mp_drawing.DrawingSpec(color=(0,128,0), thickness=1, circle_radius=1)
-                             ) 
-    # Dibujar landmarks de la mano izquierda
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(0,128,0), thickness=2, circle_radius=2)
-                             ) 
-    # Dibujar landmarks de la mano derecha
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(0,128,0), thickness=2, circle_radius=2)
-                             )
+# -----------------------------------------------------------
+# Recorrer las carpetas con los datos
+for dir_ in os.listdir(DATA_PATH):
 
-holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) # Crear modelo holistic
+    # Crear directorio para los datos procesados si no existe
+    if not os.path.exists(os.path.join(PROCESS_DATA_PATH, str(dir_))):
+        os.makedirs(os.path.join(PROCESS_DATA_PATH, str(dir_)))
+
+    # -----------------------------------------------------------
+    # Recorrer cada video en la carpeta actual
+    for file_name in os.listdir(os.path.join(DATA_PATH, dir_)):
+        file_path = os.path.join(DATA_PATH, dir_, file_name)
+
+        # Crear directorio para el video si no existe
+        video_dir_path = os.path.join(PROCESS_DATA_PATH, dir_, str(file_name).split('.')[0])
+        os.makedirs(video_dir_path, exist_ok=True)
+        
+        # Leer el video
+        cap = cv2.VideoCapture(file_path)
+        frame_num = 0
+
+        # -----------------------------------------------------------
+        # Recorrer cada frame del video y guardar sus keypoints
+        while cap.isOpened():
+
+            ret, frame = cap.read()
+            if not ret: break
+
+            # Convertir el color de BGR a RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Procesar la imagen y obtener los resultados
+            results = mp_holistic.process(frame)
+
+            # Extraer los keypoints y guardarlos 
+            if results is not None:
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(video_dir_path, str(frame_num))
+                np.save(npy_path, keypoints)
+                
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+            frame_num += 1
+        
+        cap.release()
+        cv2.destroyAllWindows()
+
---
+++ src/train_classifier.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+
+from tensorflow.keras.utils import to_categorical # type: ignore
+from sklearn.model_selection import train_test_split 
+
+PROCESS_DATA_PATH = './processed_data'
+
+# -----------------------------------------------------------
+# Obtener las categorías y mapearlas a un número
+categories = os.listdir(PROCESS_DATA_PATH)
+categories_map = {category: i for i, category in enumerate(categories)}
+print(categories_map)
+
+# Obtener el número de videos y frames por video por sena
+n_videos = len(os.listdir(os.path.join(PROCESS_DATA_PATH, categories[0])))
+n_frames = len(os.listdir(os.path.join(PROCESS_DATA_PATH, categories[0], str(0))))
+
+# -----------------------------------------------------------
+# Crear los arrays de los datos y sus correspondientes etiquetas para entrenar el modelo
+
+videos, labels = [], []
+for category in categories:
+    for video in range(n_videos):
+        frames = []
+        for frame_num in range(n_frames):
+            info = np.load(os.path.join(PROCESS_DATA_PATH, category, str(video), str(frame_num) + '.npy'))
+            frames.append(info)
+        videos.append(frames)
+        labels.append(categories_map[category])
+
+
+x = np.array(videos)
+y = to_categorical(labels).astype(int)
+print(y)
+print(x.shape)
+print(y.shape)
+
+# -----------------------------------------------------------
+# Dividir los datos en entrenamiento y prueba
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1) # 10% de los datos para prueba* 
