6e445a2ecf8fa8ba6dc4851a5c50b46f8d33289a
Juan Goez <jgoezd@unal.edu.co>
2024-06-01 11:37:04 -0500

    Updated: train_classifier.py, Ahora guarda los datos y las labels para no tener que repetir el proceso de etiquetamiento

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -7,21 +7,34 @@ import numpy as np
 
 # Librerias asociadas
 from tensorflow.keras.utils import to_categorical # type: ignore
+
 PROCESS_DATA_PATH = './processed_data'
 
 # -----------------------------------------------------------
-def load_processed_data(PROCESS_DATA_PATH):
+
+def load_processed_data(PROCESS_DATA_PATH, save_path='processed_data'):
     """
     Carga los datos procesados desde los archivos .npy y los organiza en arrays para el entrenamiento del modelo.
 
     Args:
         PROCESS_DATA_PATH (str): La ruta del directorio que contiene los datos procesados organizados por categorías.
+        save_path (str): La ruta donde se guardarán los archivos de datos y etiquetas.
 
     Returns:
         tuple: Una tupla que contiene dos elementos:
             - x (np.ndarray): Un array de forma (n_videos, n_frames, num_keypoints) que contiene los datos de los videos.
             - y (np.ndarray): Un array de etiquetas en formato categórico correspondiente a cada video.
     """
+    # Verificar si los archivos de datos ya existen
+    x_path = os.path.join(save_path, 'x_data.npy')
+    y_path = os.path.join(save_path, 'y_data.npy')
+    
+    # Si ya existen, los devuelve inmediatamente
+    if os.path.exists(x_path) and os.path.exists(y_path):
+        x = np.load(x_path)
+        y = np.load(y_path)
+        return x, y
+
     # Obtener las categorías y mapearlas a un número
     categories = os.listdir(PROCESS_DATA_PATH)
     categories_map = {category: i for i, category in enumerate(categories)}
@@ -31,6 +44,9 @@ def load_processed_data(PROCESS_DATA_PATH):
     n_videos = len(os.listdir(os.path.join(PROCESS_DATA_PATH, categories[0])))
     n_frames = len(os.listdir(os.path.join(PROCESS_DATA_PATH, categories[0], str(0))))
 
+    print(n_videos)
+    print(n_frames)
+    
     # Crear los arrays de los datos y sus correspondientes etiquetas para entrenar el modelo
     videos, labels = [], []
     for category in categories:
@@ -45,4 +61,10 @@ def load_processed_data(PROCESS_DATA_PATH):
     x = np.array(videos)
     y = to_categorical(labels).astype(int)
 
-    return x, y 
+    # Guardar los datos procesados en archivos .npy
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    np.save(x_path, x)
+    np.save(y_path, y)
+
+    return x, y
